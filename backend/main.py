2afcfec2f159c1c88bf1e80b24867900ace76285
David McDermott <dmcdermott@rosen-nxt.com>
2025-02-15 22:45:13 -0800

    Update FastAPI root path to "/api"

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
-app = FastAPI()
+app = FastAPI(root_path="/api")
 
 app.add_middleware(
     CORSMiddleware,
